e3b92f6680c62d9c6a2b623a9c234e1e36b56bce
Exan <exan@landviz.nl>
2023-01-26 19:16:32 +0100

    Tests

---
+++ tests/BucketTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Exan\Dhp;
+
+use Exan\Dhp\Bucket;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use React\EventLoop\Loop;
+
+use function Clue\React\Block\await;
+use function Clue\React\Block\awaitAll;
+
+class BucketTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testLimit()
+    {
+        $bucket = new Bucket(Loop::get(), 2, 1);
+
+        $spy = Mockery::spy(function() {});
+
+        $start = microtime(true);
+
+        awaitAll([$bucket->run($spy), $bucket->run($spy)]);
+
+        $mid = microtime(true);
+        $spy->shouldHaveBeenCalled()->twice();
+
+        await($bucket->run($spy));
+
+        $end = microtime(true);
+
+        $this->assertGreaterThanOrEqual(1, $end - $start);
+        $this->assertGreaterThanOrEqual(0.99, $end - $mid);
+    }
+
+    public function testRejectsPromiseOnError()
+    {
+        $bucket = new Bucket(Loop::get(), 2, 1);
+
+        $errorThrower = function () {
+            throw new \Exception('::exception::');
+        };
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('::exception::');
+
+        await($bucket->run($errorThrower));
+    }
+}
