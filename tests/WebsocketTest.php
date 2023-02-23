ed45469866a86e1089647ca0ffadba35535f9805
Exanlv <exan@landviz.nl>
2023-02-23 21:23:30 +0100

    Add local websocket server to connect to

--- tests/WebsocketTest.php
+++ tests/WebsocketTest.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Tests\Exan\Fenrir;
 
+use Exan\Fenrir\Const\WebsocketEvents;
 use Exan\Fenrir\Exceptions\Websocket\ConnectionNotInitializedException;
 use Exan\Fenrir\Websocket;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
+use Ratchet\RFC6455\Messaging\MessageInterface;
+use React\Promise\Promise;
+
+use function Clue\React\Block\await;
 
 class WebsocketTest extends TestCase
 {
@@ -20,5 +25,43 @@ class WebsocketTest extends TestCase
 
         $this->expectException(ConnectionNotInitializedException::class);
         $websocket->send('::message::');
+    }
+
+    public function testItConnectsToWsAndHasIOMessagesWithoutBucket()
+    {
+        $websocket = new Websocket(10, new NullLogger());
+
+        await($websocket->open('ws://localhost:8080/echo'));
+
+        $message = await(new Promise(function (callable $resolve) use ($websocket) {
+            $websocket->on(WebsocketEvents::MESSAGE, function (MessageInterface $message) use ($resolve) {
+                $resolve($message);
+            });
+
+            $websocket->send('::message::', false);
+        }));
+
+        $this->assertEquals('::message::', (string) $message);
+
+        $websocket->close(1001, '::reason::');
+    }
+
+    public function testItConnectsToWsAndHasIOMessagesWithBucket()
+    {
+        $websocket = new Websocket(10, new NullLogger());
+
+        await($websocket->open('ws://localhost:8080/echo'));
+
+        $message = await(new Promise(function (callable $resolve) use ($websocket) {
+            $websocket->on(WebsocketEvents::MESSAGE, function (MessageInterface $message) use ($resolve) {
+                $resolve($message);
+            });
+
+            $websocket->send('::message::', true);
+        }));
+
+        $this->assertEquals('::message::', (string) $message);
+
+        $websocket->close(1001, '::reason::');
     }
 }
---
+++ tests/WebsocketTestServer.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Ratchet\App('localhost', 8080);
+
+$app->route('/echo', new \Ratchet\Server\EchoServer, array('*'));
+
+$app->run();
