3b1e80c7b74a1e7ae2eb449810603ef6fe3187a3
Exanlv <exan@landviz.nl>
2023-03-20 23:25:37 +0100

    Remove dead comments

--- fakes/RestFake.php
+++ fakes/RestFake.php
@@ -13,21 +13,10 @@ class RestFake
 {
     public static function get(): Rest|Mock
     {
-        // $reflectionClass = new ReflectionClass(Rest::class);
-        // $instance = ;
-        // $reflectionProperty = $reflection->getProperty('changeMe');
-        // $reflectionProperty->setValue($instance, 33);
-
-        // var_dump($reflectionProperty->getValue($instance));
-        // $restMock = Mockery::mock(Rest::class);
-
-        // $instance = $reflectionClass->newInstanceWithoutConstructor();
-
-
         $reflection = new ReflectionClass(Rest::class);
+
+        /**  @var Rest */
         $instance = $reflection->newInstanceWithoutConstructor();
-        // $reflectionProperty = $reflection->getProperty('changeMe');
-        // $reflectionProperty->setValue($instance, 33);
 
         foreach ($reflection->getProperties() as $property) {
             if (!$property->isPublic()) {
