dca6c5838c544492f5c3221aa75cee5aca5a715b
Exanlv <exan@landviz.nl>
2023-03-28 15:11:00 +0200

    Clean up testing code

--- util/missing-strict-types.sh
+++ util/missing-strict-types.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-grep -riL "^declare(strict_types=1)" src fakes tests;
-grep -riL "^declare(strict_types=1)" src fakes tests | wc -l | xargs test 0 -eq;
+grep -riL "^declare(strict_types=1)" src fakes tests phpcs;
+grep -riL "^declare(strict_types=1)" src fakes tests phpcs | wc -l | xargs test 0 -eq;
