1a0ea0b41f5d7fb325ee984e93f4c2911f86dbb2
Exanlv <exan@landviz.nl>
2023-03-28 15:14:52 +0200

    Fix dir path

--- util/missing-strict-types.sh
+++ util/missing-strict-types.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-grep -riL "^declare(strict_types=1)" src fakes tests phpcs;
-grep -riL "^declare(strict_types=1)" src fakes tests phpcs | wc -l | xargs test 0 -eq;
+grep -riL "^declare(strict_types=1)" src fakes tests phpcs/Sniffs;
+grep -riL "^declare(strict_types=1)" src fakes tests phpcs/Sniffs | wc -l | xargs test 0 -eq;
