934cc6d0da0437c10d697cb15a1705c7bfc8c066
Exanlv <51094537+Exanlv@users.noreply.github.com>
2023-06-17 18:22:55 +0200

    Add extension support (#49)

---
+++ src/Exceptions/Extension/ExtensionNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ragnarok\Fenrir\Exceptions\Extension;
+
+use Exception;
+use Psr\Container\ContainerExceptionInterface;
+
+class ExtensionNotFoundException extends Exception
+{
+}
--- tests/DiscordTest.php
+++ tests/DiscordTest.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace Tests\Ragnarok\Fenrir;
 
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\MockInterface;
 use Ragnarok\Fenrir\Bitwise\Bitwise;
 use Ragnarok\Fenrir\Discord;
+use Ragnarok\Fenrir\Exceptions\Extension\ExtensionNotFoundException;
 use Ragnarok\Fenrir\InteractionHandler;
 use Ragnarok\Fenrir\Rest\Rest;
-use PHPUnit\Framework\TestCase;
+use Ragnarok\Fenrir\Extension\Extension;
 use Ragnarok\Fenrir\Gateway\Connection;
 
-class DiscordTest extends TestCase
+class DiscordTest extends MockeryTestCase
 {
     public function testItInitializesGateway(): void
     {
@@ -65,5 +69,69 @@ class DiscordTest extends TestCase
         foreach ($requirements as $requirement) {
             $this->assertArrayHasKey($requirement, $debugInfo);
         }
+    }
+
+    public function testItCanInitializeExtensions(): void
+    {
+        /**
+         * @var Extension|MockInterface
+         */
+        $extension = Mockery::mock(Extension::class);
+
+        $discord = new Discord('TOKEN');
+
+        $extension->expects()
+            ->initialize()
+            ->with($discord)
+            ->once();
+
+        $discord->registerExtension($extension);
+    }
+
+    public function testItTellsYouWhetherExtensionsAreInstalled(): void
+    {
+        $extension = new class implements Extension {
+            public function initialize(Discord $discord): void
+            {
+            }
+        };
+
+        $discord = new Discord('TOKEN');
+
+        $this->assertFalse($discord->hasExtension(get_class($extension)));
+
+        $discord->registerExtension($extension);
+
+        $this->assertTrue($discord->hasExtension(get_class($extension)));
+    }
+
+    public function testItReturnsTheExtension(): void
+    {
+        $extension = new class implements Extension {
+            public function initialize(Discord $discord): void
+            {
+            }
+        };
+
+        $discord = new Discord('TOKEN');
+
+        $discord->registerExtension($extension);
+
+        $this->assertEquals($extension, $discord->getExtension(get_class($extension)));
+    }
+
+    public function testItYellsWhenYouTryToReturnANonRegisteredExtension(): void
+    {
+        $extension = new class implements Extension {
+            public function initialize(Discord $discord): void
+            {
+            }
+        };
+
+        $discord = new Discord('TOKEN');
+
+        $this->expectException(ExtensionNotFoundException::class);
+
+        $this->assertEquals($extension, $discord->getExtension(get_class($extension)));
     }
 }
