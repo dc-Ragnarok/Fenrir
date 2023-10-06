fe26238dd81652e4847af02da18d431f60b1a390
Exanlv <exan@landviz.nl>
2023-10-06 19:51:20 +0200

    Update example for raw events

--- examples/02-listening-to-raw-events.php
+++ examples/02-listening-to-raw-events.php
@@ -2,10 +2,27 @@
 
 use Ragnarok\Fenrir\Bitwise\Bitwise;
 use Ragnarok\Fenrir\Constants\Events;
+use Ragnarok\Fenrir\Constants\OpCodes;
 use Ragnarok\Fenrir\Discord;
+use Ragnarok\Fenrir\Gateway\Handlers\GatewayEvent;
 use Ragnarok\Fenrir\Gateway\Objects\Payload;
 
 require './vendor/autoload.php';
+
+class RawHandler extends GatewayEvent {
+    public static function getEventName(): string
+    {
+        return OpCodes::EVENTS; // Or any other OP code
+    }
+
+    public function execute(): void
+    {
+        // ...
+        // $this->payload
+        // $this->connectionInterface
+        // $this->logger
+    }
+}
 
 $discord = new Discord('TOKEN');
 
@@ -13,8 +30,6 @@ $discord
     ->withGateway(new Bitwise())// Enable your desired Gateway intents
     ->withRest();
 
-$discord->gateway->events->on(Events::RAW, static function (Payload $payload) {
-    echo 'Received event ', $payload->t, PHP_EOL;
-});
+$discord->gateway->raw->register(RawHandler::class);
 
 $discord->gateway->open(); // Nothing after this line is executed
