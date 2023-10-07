2750841f4b6816e5e72cf37ca3cea6879472aeab
Exanlv <exan@landviz.nl>
2023-10-07 10:24:48 +0200

    Add unavailable guilds to Ready event

--- src/Gateway/Events/Ready.php
+++ src/Gateway/Events/Ready.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ragnarok\Fenrir\Gateway\Events;
 
-use Ragnarok\Fenrir\Attributes\Partial;
 use Ragnarok\Fenrir\Gateway\Objects\Payload;
 use Ragnarok\Fenrir\Parts\Application;
 use Ragnarok\Fenrir\Parts\User;
@@ -13,10 +12,12 @@ class Ready extends Payload
 {
     public int $v;
     public User $user;
+
+    /** @var \Ragnarok\Fenrir\Parts\UnavailableGuild[] */
+    public array $guilds;
+
     public string $session_id;
     public string $resume_gateway_url;
     public array $shard;
-
-    #[Partial]
     public Application $application;
 }
---
+++ src/Parts/UnavailableGuild.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ragnarok\Fenrir\Parts;
+
+class UnavailableGuild
+{
+    public string $id;
+    public bool $unavailable;
+}
