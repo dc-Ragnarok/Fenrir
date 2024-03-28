2576386f382f075514f50dfd7c5498b280870ff9
Exanlv <51094537+Exanlv@users.noreply.github.com>
2024-03-28 10:47:21 +0100

    Remove deprecated application ID from global & guild command extensions (#86)

--- src/Command/GlobalCommandExtension.php
+++ src/Command/GlobalCommandExtension.php
@@ -11,13 +11,6 @@ use Ragnarok\Fenrir\Gateway\Events\InteractionCreate;
  */
 class GlobalCommandExtension extends CommandExtension
 {
-    public function __construct(?string $applicationId = null)
-    {
-        if (!is_null($applicationId)) {
-            trigger_error('Providing an application ID is no longer required and will be removed in a later version');
-        }
-    }
-
     protected function emitInteraction(InteractionCreate $interaction): bool
     {
         return !isset($interaction->data->guild_id);
