6368a1a1d34c2c3fea08e579ba39f75f77b0554a
Exanlv <51094537+Exanlv@users.noreply.github.com>
2024-09-26 12:34:07 +0200

    Add doc comment

--- src/Parts/MessageInteraction.php
+++ src/Parts/MessageInteraction.php
@@ -14,6 +14,10 @@ class MessageInteraction
     public string $id;
     public InteractionType $type;
     public User $user;
+    /**
+     * @var string[]
+     * @see https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-authorizing-integration-owners-object
+     */
     public array $authorizing_integration_owners;
     public ?string $original_response_message_id;
     public ?string $interacted_message_id;
