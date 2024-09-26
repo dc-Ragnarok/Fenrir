771042e3e916871090f9d5dc92fb25d0f3871fac
Exanlv <exan@landviz.nl>
2024-09-26 11:33:20 +0200

    Add context to interaction

--- src/Parts/Interaction.php
+++ src/Parts/Interaction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ragnarok\Fenrir\Parts;
 
+use Ragnarok\Fenrir\Enums\InteractionContextType;
 use Ragnarok\Fenrir\Enums\InteractionType;
 
 class Interaction
@@ -24,4 +25,5 @@ class Interaction
     public ?string $locale;
     public string $guild_locale;
     public Channel $channel;
+    public InteractionContextType $context;
 }
