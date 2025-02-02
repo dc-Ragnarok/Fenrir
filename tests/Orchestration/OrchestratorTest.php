6c907b39a9ea2885dc8bd5ad9c9df51a94a77a4a
Exanlv <exan@landviz.nl>
2025-02-02 14:51:34 +0100

    Lay groundwork for asserting it refretches allowance

--- tests/Orchestration/OrchestratorTest.php
+++ tests/Orchestration/OrchestratorTest.php
@@ -18,8 +18,11 @@ use Ragnarok\Fenrir\Rest\Gateway as HttpGateway;
 
 class OrchestratorTest extends TestCase
 {
+    private const EXPECT_SPAWN = 'expect_spawn';
+    private const EXPECT_WAIT = 'expect_wait';
+
     /** @dataProvider orchestrationDataProvider */
-    public function testItSpawnsBasedOnGatewayBotInfo(array $gatewayBots, array $expectedSpawns): void
+    public function testItSpawnsBasedOnGatewayBotInfo(array $gatewayBots, array $expectations): void
     {
         $loop = new LoopFake();
         /** @var HttpGateway&MockInterface */
@@ -43,10 +46,18 @@ class OrchestratorTest extends TestCase
 
         $orchestrator->init();
 
-        foreach ($expectedSpawns as $expectedSpawn) {
-            $this->assertEquals($expectedSpawn, $spawned);
-            $spawned = [];
-            $loop->runTimers();
+        $firstExpectation = array_shift($expectations);
+        $this->assertEquals($firstExpectation['data'], $spawned);
+        $spawned = [];
+
+        foreach ($expectations as $expectation) {
+            switch ($expectation['type']) {
+                case self::EXPECT_SPAWN: {
+                    $loop->runTimers();
+                    $this->assertEquals($expectation['data'], $spawned);
+                    $spawned = [];
+                }
+            }
         }
 
         $this->assertEmpty($spawned);
@@ -86,27 +97,132 @@ class OrchestratorTest extends TestCase
                         5,
                     ),
                 ],
-                'expectedSpawns' => [
+                'expectations' => [
                     [
-                        [0, 15],
-                        [1, 15],
-                        [2, 15],
-                        [3, 15],
-                        [4, 15],
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [0, 15],
+                            [1, 15],
+                            [2, 15],
+                            [3, 15],
+                            [4, 15],
+                        ]
                     ],
                     [
-                        [5, 15],
-                        [6, 15],
-                        [7, 15],
-                        [8, 15],
-                        [9, 15],
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [5, 15],
+                            [6, 15],
+                            [7, 15],
+                            [8, 15],
+                            [9, 15],
+                        ]
                     ],
                     [
-                        [10, 15],
-                        [11, 15],
-                        [12, 15],
-                        [13, 15],
-                        [14, 15],
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [10, 15],
+                            [11, 15],
+                            [12, 15],
+                            [13, 15],
+                            [14, 15],
+                        ]
+                    ],
+                ],
+            ],
+
+            'Less ideal conditions, total % concurrency == 3' => [
+                'gatewayBots' => [
+                    self::getGatewayBot(
+                        18,
+                        18,
+                        18,
+                        Carbon::now()->addMinutes(5),
+                        5,
+                    ),
+                ],
+                'expectations' => [
+                    [
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [0, 18],
+                            [1, 18],
+                            [2, 18],
+                            [3, 18],
+                            [4, 18],
+                        ]
+                    ],
+                    [
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [5, 18],
+                            [6, 18],
+                            [7, 18],
+                            [8, 18],
+                            [9, 18],
+                        ]
+                    ],
+                    [
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [10, 18],
+                            [11, 18],
+                            [12, 18],
+                            [13, 18],
+                            [14, 18],
+                        ]
+                    ],
+                    [
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [15, 18],
+                            [16, 18],
+                            [17, 18],
+                        ]
+                    ],
+                ],
+            ],
+
+            'It spawns only to the limit of remaining before waiting for reset, and fetching new allowance' => [
+                'gatewayBots' => [
+                    self::getGatewayBot(
+                        18,
+                        18,
+                        15,
+                        Carbon::now()->addMinutes(5),
+                        5,
+                    ),
+                ],
+                'expectations' => [
+                    [
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [0, 18],
+                            [1, 18],
+                            [2, 18],
+                            [3, 18],
+                            [4, 18],
+                        ]
+                    ],
+                    [
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [5, 18],
+                            [6, 18],
+                            [7, 18],
+                            [8, 18],
+                            [9, 18],
+                        ]
+                    ],
+                    [
+                        'type' => self::EXPECT_SPAWN,
+                        'data' => [
+                            [10, 18],
+                            [11, 18],
+                            [12, 18],
+                            [13, 18],
+                            [14, 18],
+                        ]
                     ],
                 ],
             ],
